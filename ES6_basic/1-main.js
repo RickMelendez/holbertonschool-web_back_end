6a2c8f97b76cc17197507f16ca165c530ae17d5a
RickMelendez <rickmelendez001@gmail.com>
2024-07-17 21:45:36 -0400

    test

--- ES6_basic/1-main.js
+++
@@ -1,4 +0,0 @@
-import taskBlock from './1-block-scoped.js';
-
-console.log(taskBlock(true));   // Expected output: [ true, false ]
-console.log(taskBlock(false));  // Expected output: [ false, true ]
