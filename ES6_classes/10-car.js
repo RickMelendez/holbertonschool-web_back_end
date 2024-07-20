a5b3a700a382f25bbc4f63c8c4bd32d3c9c624aa
RickMelendez <rickmelendez001@gmail.com>
2024-07-19 22:11:05 -0400

    10

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,18 @@
+const cloneSymbol = Symbol('clone');
+
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  [cloneSymbol]() {
+    return new Car(this._brand, this._motor, this._color);
+  }
+
+  cloneCar() {
+    const ClonedClass = this.constructor[cloneSymbol];
+    return ClonedClass.call(this);
+  }
+}
