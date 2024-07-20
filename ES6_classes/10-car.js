2e3ce44ae8bda49057f4dd3784d9557cec824254
RickMelendez <rickmelendez001@gmail.com>
2024-07-19 22:55:51 -0400

    10

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,14 +1,14 @@
 class Car {
-    constructor(brand, motor, color) {
-      this._brand = brand;
-      this._motor = motor;
-      this._color = color;
-    }
-  
-    cloneCar() {
-      return new Car(this._brand, this._motor, this._color);
-    }
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
-  
-  export default Car;
-  
+
+  cloneCar() {
+    // Return an instance of the same class as the original object
+    return new this.constructor(this._brand, this._motor, this._color);
+  }
+}
+
+export default Car;
