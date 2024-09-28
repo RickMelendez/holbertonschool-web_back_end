4d110ab1da01d71605fc7987fde29c886fdb66f7
RickMelendez <rickmelendez001@gmail.com>
2024-09-28 15:12:34 -0400

    task1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,19 +1,11 @@
-// Display the welcome message
-console.log("Welcome to Holberton School, what is your name?");
-
-// Listen for user input
-process.stdin.on('data', (data) => {
-  // Trim the input to remove any extra newline or spaces
-  const name = data.toString().trim();
-  
-  // Display the user's name
-  console.log(`Your name is: ${name}`);
-  
-  // Exit the process
-  process.exit();
+// Program that gets input from user and writes it to stdout
+process.stdin.setEncoding('utf-8');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) process.stdout.write(`Your name is: ${name}`);
 });
 
-// Listen for process exit event to display the closing message
-process.on('exit', () => {
-  console.log("This important software is now closing");
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
