5f10a7ebcb306f242f0ea31be2398f484e1f4a8b
RickMelendez <rickmelendez001@gmail.com>
2024-12-09 23:40:01 -0400

    fix errors

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,27 +1,24 @@
 #!/usr/bin/env python3
-"""
-Simple pagination for a dataset
-"""
+
+"""this file contains Server class"""
+
 
 import csv
+import math
 from typing import List, Tuple
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Returns a tuple of start and end indexes based on page and page_size."""
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
-    return start_index, end_index
 
 class Server:
-    """Server class to paginate a database of popular baby names."""
-    
+    """Server class to paginate a database of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset"""
+        """Cached dataset
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -31,14 +28,21 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Returns a page from the dataset"""
+        """this function gets the page"""
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
-        dataset = self.dataset()
-        start_index, end_index = index_range(page, page_size)
+        start_index, end_index = self.index_range(page, page_size)
+        data = self.dataset()
 
-        if start_index >= len(dataset):
+        if start_index >= len(data):
             return []
-        
-        return dataset[start_index:end_index]
+
+        return data[start_index:end_index]
+
+    @staticmethod
+    def index_range(page, page_size) -> Tuple[int, int]:
+        """this function returns a tuple with the start and end indexes"""
+        start_index = (page - 1) * page_size
+        end_index = start_index + page_size
+        return (start_index, end_index)
--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -1,28 +1,24 @@
 #!/usr/bin/env python3
-"""
-Hypermedia pagination for a dataset
-"""
+
+"""this file contains Server class"""
+
 
 import csv
 import math
-from typing import List, Dict, Tuple
+from typing import List, Tuple, Dict
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Returns a tuple of start and end indexes based on page and page_size."""
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
-    return start_index, end_index
 
 class Server:
-    """Server class to paginate a database of popular baby names."""
-    
+    """Server class to paginate a database of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset"""
+        """Cached dataset
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -32,28 +28,40 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Returns a page from the dataset"""
+        """this function gets the page"""
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
-        dataset = self.dataset()
-        start_index, end_index = index_range(page, page_size)
+        start_index, end_index = self.index_range(page, page_size)
+        data = self.dataset()
 
-        if start_index >= len(dataset):
+        if start_index >= len(data):
             return []
-        
-        return dataset[start_index:end_index]
+
+        return data[start_index:end_index]
+
+    @staticmethod
+    def index_range(page, page_size) -> Tuple[int, int]:
+        """this function returns a tuple with the start and end indexes"""
+        start_index = (page - 1) * page_size
+        end_index = start_index + page_size
+        return (start_index, end_index)
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
-        """Returns hypermedia pagination information."""
-        data = self.get_page(page, page_size)
-        total_pages = math.ceil(len(self.dataset()) / page_size)
+        """this function returns a dict with info of the page"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        start, end = self.index_range(page, page_size)
+
+        total_pages = int(len(self.dataset()) / 10)
+        if (page_size > 0):
+            total_pages = int(len(self.dataset()) / page_size)
 
         return {
-            "page_size": len(data),
-            "page": page,
-            "data": data,
-            "next_page": page + 1 if page + 1 <= total_pages else None,
-            "prev_page": page - 1 if page > 1 else None,
-            "total_pages": total_pages
+            'page_size': len(self.get_page(page, page_size)),
+            'page': page,
+            'data': self.get_page(page, page_size),
+            'next_page': page + 1 if len(self.dataset()) > end else None,
+            'prev_page': page - 1 if page > 1 else None,
+            'total_pages': total_pages
         }
